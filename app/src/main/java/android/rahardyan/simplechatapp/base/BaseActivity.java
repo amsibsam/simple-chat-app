bca6f8a958e0b1a22f41de6f335ac1b7282a4ef2
amsibsam <amsibsam@gmail.com>
2017-06-04 14:38:34 +0700

    make title optional for progressdialog

--- app/src/main/java/android/rahardyan/simplechatapp/base/BaseActivity.java
+++ app/src/main/java/android/rahardyan/simplechatapp/base/BaseActivity.java
@@ -88,10 +88,13 @@ public abstract class BaseActivity extends AppCompatActivity {
 
     private void setUpProgressDialog() {
         progressDialog = new ProgressDialog(this);
-        progressDialog.setTitle(R.string.loading);
         progressDialog.setMessage(getString(R.string.loading));
         progressDialog.setCancelable(false);
         progressDialog.setIndeterminate(true);
+    }
+
+    public void setProgressDialogTitle(String title) {
+        progressDialog.setTitle(title);
     }
 
     private void setUpAlertDialog() {
